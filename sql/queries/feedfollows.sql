f26378fcd8064d936b4686b4128feafbd73d19ae
Gallus-gallusdomesticus <stephenefelson1999@gmail.com>
2026-04-10 11:18:14 +1000

    add sqlc generate for createfeedfollows

---
+++ sql/queries/feedfollows.sql
@@ -0,0 +1,20 @@
+-- name: CreateFeedFollows :many
+WITH insert_feed_follow AS (
+    INSERT INTO feed_follows(id, created_at, updated_at, user_id, feed_id)
+    VALUES (
+        $1,
+        $2,
+        $3,
+        $4,
+        $5
+    )
+    RETURNING *
+)
+SELECT insert_feed_follow.*,
+    users.name AS user_name,
+    feeds.name AS feed_name
+    FROM insert_feed_follow
+    INNER JOIN users
+    ON insert_feed_follow.user_id = users.id
+    INNER JOIN feeds
+    ON insert_feed_follow.feed_id = feeds.id;
